80c01aa6a79b77aa6869abb201a5b3334982d13a
Boaz Crezee <bjcrezee@gmail.com>
2025-06-25 22:22:59 +0200

    installed laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductOrderController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Article;
-use App\Http\Controllers\ArticleController;
 
-// Route::get('/', function () {
-//     // Take the 3 newest posts
-//     $latestArticles = Article::orderBy('published_at', 'desc')->take(3)->get();
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//     return view('welcome', compact('latestArticles'));
-// })->name('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::redirect('/', '/categories');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Resource routes of the base pages. For more info on Resource Routes
-// Route::resource('/articles', ArticleController::class);
-// Route::resource('/products', ProductController::class);
-// Route::resource('/orders', ProductOrderController::class);
-Route::resource('/categories', CategoryController::class);
+require __DIR__.'/auth.php';
