dc7fea91cc3efa6f4705d0c3b8809d500e0e66c5
Boaz Crezee <bjcrezee@gmail.com>
2025-06-26 20:06:39 +0200

    go home label

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 <x-recursor-page
     title="Oh no! We couldn't find that page"
     code="404"
-    buttonUrl="{{ url('/') }}" button="Go back"
+    buttonUrl="{{ url('/') }}" button="Go home"
     :posts="['post of last week', 'post I deleted', 'post on the obscure server we don’t talk about']"
     supportMessage="If the issue persists, please"
 />
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 <x-recursor-page
     title="Oh no! We are having some technical difficulties. We hope to get this sorted out soon."
     code="500"
-    buttonUrl="{{ url('/') }}" button="Go back"
+    buttonUrl="{{ url('/') }}" button="Go home"
     :posts="['post of last week', 'post I deleted', 'post on the obscure server we don’t talk about']"
     supportMessage="If the issue persists, please"
 />
